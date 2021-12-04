817a8ef6b4796911b10a01f3a628d1a3c2d65d8f
うぃーうぉん <80945906+MWindows99@users.noreply.github.com>
2021-12-04 21:39:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     consumer_key = "xxxx"
     consumer_secret = "xxxx"
     access_token = "xxxx"
-    access_token_secret ="xxxx"
+    access_token_secret = "xxxx"
     
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -22,5 +22,5 @@ def main():
         except:
             pass
     
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
